8f4badfda1764dceadffc5deb5bb2e556faad5c0
Arran Hobson Sayers <ahobsonsayers@gmail.com>
2026-02-01 04:46:28 +0000

    Add chown to entrypoint

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,14 +1,19 @@
 #!/bin/bash
 set -e
 
+echo "Installing apt packages"
 if [ -n "$APT_PACKAGES" ]; then
 	sudo apt-get update
 	sudo apt-get install -y $APT_PACKAGES
 	sudo rm -rf /var/lib/apt/lists/*
 fi
 
+echo "Installing bre packages"
 if [ -n "$BREW_PACKAGES" ]; then
 	brew install $BREW_PACKAGES
 fi
+
+echo "Fixing permissions"
+sudo chown -R $(id -u):$(id -g) "$HOME"
 
 exec "$@"
